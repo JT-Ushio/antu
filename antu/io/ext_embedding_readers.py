8c0e13c37ce33a581b167bac972124618f956b27
Tao Ji <taoji.cs@gmail.com>
2021-01-09 23:35:59 +0800

    Fix(antu.io): robust embedding reader.

--- antu/io/ext_embedding_readers.py
+++ antu/io/ext_embedding_readers.py
@@ -10,6 +10,15 @@ def glove_reader(file_path: str, only_word: bool = False) -> Tuple[List[str], Li
         vector = []
         if '.gz' != file_path[-3:]:
             with open(file_path, 'r') as fp:
+                w_list = fp.readline().strip().split(' ')
+                if len(w_list) == 2:
+                    w_dim = int(w_list[1])
+                else:
+                    w_dim = len(w_list)-1
+                    word.append(w_list[0])
+                    if not only_word:
+                        vector.append(list(map(float, w_list[1:])))
+
                 for w in fp:
                     w_list = w.strip().split(' ')
                     word.append(w_list[0])
@@ -18,6 +27,15 @@ def glove_reader(file_path: str, only_word: bool = False) -> Tuple[List[str], Li
             return word if only_word else (word, vector)
         else:
             with gzip.open(file_path, 'rt') as fp:
+                w_list = fp.readline().strip().split(' ')
+                if len(w_list) == 2:
+                    w_dim = int(w_list[1])
+                else:
+                    w_dim = len(w_list)-1
+                    word.append(w_list[0])
+                    if not only_word:
+                        vector.append(list(map(float, w_list[1:])))
+
                 for w in fp:
                     w_list = w.strip().split(' ')
                     word.append(w_list[0])
@@ -34,7 +52,14 @@ def fasttext_reader(file_path: str, only_word: bool = False) -> Tuple[List[str],
         vector = []
         if '.gz' != file_path[-3:]:
             with open(file_path, 'r') as fp:
-                w_dim = int(fp.readline().strip().split(' ')[1])
+                w_list = fp.readline().strip().split(' ')
+                if len(w_list) == 2:
+                    w_dim = int(w_list[1])
+                else:
+                    w_dim = len(w_list)-1
+                    word.append(w_list[0])
+                    if not only_word:
+                        vector.append(list(map(float, w_list[1:])))
                 for w in fp:
                     w_list = w.strip().split(' ')
                     if len(w_list)-1 != w_dim:
@@ -45,7 +70,14 @@ def fasttext_reader(file_path: str, only_word: bool = False) -> Tuple[List[str],
             return word if only_word else (word, vector)
         else:
             with gzip.open(file_path, 'rt') as fp:
-                w_dim = int(fp.readline().strip().split(' ')[1])
+                w_list = fp.readline().strip().split(' ')
+                if len(w_list) == 2:
+                    w_dim = int(w_list[1])
+                else:
+                    w_dim = len(w_list)-1
+                    word.append(w_list[0])
+                    if not only_word:
+                        vector.append(list(map(float, w_list[1:])))
                 for w in fp:
                     w_list = w.strip().split(' ')
                     if len(w_list)-1 != w_dim:
